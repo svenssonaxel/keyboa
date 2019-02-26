8963bc1203a2b2d10e7fd5e9267f36c1c516d7be
Axel Svensson <mail@axelsvensson.com>
2019-02-26 18:00:20 +0100

    stdbool

--- common.h
+++ common.h
@@ -5,9 +5,7 @@
 #define WINVER 0x0500
 #include <windows.h>
 
-typedef unsigned __int8 bool;
-#define true ((bool)(1==1))
-#define false ((bool)(1==0))
+#include <stdbool.h>
 
 #ifndef LLKHF_LOWER_IL_INJECTED
 #define LLKHF_LOWER_IL_INJECTED 0x02
